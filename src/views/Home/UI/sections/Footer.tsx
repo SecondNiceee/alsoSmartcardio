0d49a62ff4f4a53a7c80eb2381430ce8f4f9a0d5
SecondNiceee <col1596321@gmail.com>
2025-01-17 16:22:06 +0300

    fix mobile contacts

--- src/views/Home/UI/sections/Footer.tsx
+++ src/views/Home/UI/sections/Footer.tsx
@@ -4,24 +4,24 @@ import PoliticBlock from "../components/PoliticsBlock";
 const Footer = () => {
   return (
     <footer className="section bg-black">
-      <div className="container pb-[30px] flex flex-col gap-5">
-        <div className="flex justify-between gap-[15px]">
+      <div className="container  pb-[30px] flex flex-col gap-5">
+        <div className="flex flex-col md:flex-row justify-between gap-[15px]">
           <div className="flex flex-col">
-            <p className="p text-[15px] text-[#d1d1d1] text-left">
+            <p className="p text-[15px] text-[#d1d1d1] text-center md:text-left">
               ООО "АРИТМЕД"
             </p>
-            <p className="p text-[15px] text-[#d1d1d1] text-left">
+            <p className="p text-[15px] text-[#d1d1d1] text-center md:text-left">
               ОГРН 1227700287330
             </p>
-            <p className="p text-[15px] text-[#d1d1d1] text-left">
+            <p className="p text-[15px] text-[#d1d1d1] text-center md:text-left">
               ИНН 9726013334
             </p>
-            <p className="p text-[15px] text-[#d1d1d1] text-left">
+            <p className="p text-[15px] text-[#d1d1d1] text-center md:text-left">
               Адрес электронной почты: support@smartcardio.ru
             </p>
           </div>
-          <div className="flex flex-col max-w-[600px]">
-            <p className="p text-[15px] text-[#d1d1d1] text-right">
+          <div className="flex flex-col md:max-w-[600px] w-[100%] items-center justify-center">
+            <p className="p text-[15px] text-[#d1d1d1] text-center md:text-right">
               Не является медицинским устройством, перед применением
               проконсультируйтесь со специалистом. Для расшифровки результата
               так же обратитесь к специалисту. Обращаем Ваше внимание, что
