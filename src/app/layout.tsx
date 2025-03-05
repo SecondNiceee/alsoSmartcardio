6047afa041e6c3fb947b116cbcc668f69acd28c8
SecondNiceee <col1596321@gmail.com>
2025-03-05 22:23:30 +0300

    metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,29 @@ import Head from "next/head";
 import ServerStartApp from "@/features/Home/ServerStartApp";
 
 const inter = Inter({ variable: "--font4", subsets: ["cyrillic"] });
+
+
+const yandexMetrikaScript = `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {
+    if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+  k.async=1;
+  k.src=r;
+  a.parentNode.insertBefore(k,a)
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(100169482, "init", {
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true,
+  webvisor:true
+});
+`;
+
+
 const raleway = Raleway({
   variable: "--third-family",
   subsets: ["cyrillic"],
@@ -90,6 +113,13 @@ export default function RootLayout({
           <LayoutCart />
           <LayoutBuyingPopup />
         </ReduxProvider>
+
+
+        <script
+            dangerouslySetInnerHTML={{
+              __html: yandexMetrikaScript
+            }}
+          />
 
 
         <script dangerouslySetInnerHTML={{__html : `
