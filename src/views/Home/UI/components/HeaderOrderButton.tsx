8b947c681ce3b00b0c6dcf19573dae90770b1d63
SecondNiceee <col1596321@gmail.com>
2025-05-23 20:05:17 +0300

    sad

--- src/views/Home/UI/components/HeaderOrderButton.tsx
+++ src/views/Home/UI/components/HeaderOrderButton.tsx
@@ -1,22 +1,14 @@
-import { setRegistrationPopup } from '@/entities/auth/model/authSlice';
-import { useAppDispatch } from '@/shared/models/reduxHooks';
+'use client'
+import { routes } from '@/shared/config/routes';
 import OrderButton from '@/shared/UI/OrderButton/OrderButton';
 import React from 'react';
 
-const HeaderOrderButton = () => {
-
-    const dispatch = useAppDispatch();
+const FooterOrderButton = () => {
     return (
-
-          <OrderButton
-            onClick={() => {
-              dispatch(setRegistrationPopup(true))
-            }}
-            className={"header__order-button !bg-liner !sticky top-[20px] right-[20px]"}
-          >
-            <p>Личный кабинет</p>
-          </OrderButton>
+    <OrderButton link={routes.store} className='!bg-liner w-[100%] py-[11px] md:py-[18px] px-[14px] gradient-hover'>
+        <p className='clamp(1.188rem, 0.902rem + 1.43vw, 2.188rem)'>Заказать</p>
+    </OrderButton>
     );
 };
 
-export default React.memo(HeaderOrderButton);
+export default FooterOrderButton;
