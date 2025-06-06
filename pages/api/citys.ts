c7b3d1bc2b578ed3128923e8b0c5ed2100f325a0
SecondNiceee <col1596321@gmail.com>
2025-06-07 01:46:39 +0300

    asd

--- pages/api/citys.ts
+++ pages/api/citys.ts
@@ -41,8 +41,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(200).send(text);
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: {error} });
   }
 }
 
